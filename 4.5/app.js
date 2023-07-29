75cec39e152e02418c0f57a153e9aab0bd258202
Jason <github@jasongetsemail.com>
2023-07-29 17:32:07 -0400

    Complete the templates and logic for adding items Looks ugly af but I'm not a designer

--- 4.5/app.js
+++ 4.5/app.js
@@ -2,13 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
+let todayList = [];
+let weekList = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("today.ejs", { page: "the Day" });
+  res.render("today.ejs", { page: "the Day", list: todayList });
 });
 
 app.get("/today", function (req, res) {
@@ -16,7 +18,17 @@ app.get("/today", function (req, res) {
 });
 
 app.get("/week", function (req, res) {
-  res.render("week.ejs", { page: "the Week" });
+  res.render("week.ejs", { page: "the Week", list: weekList });
+});
+
+app.post("/", function (req, res) {
+  todayList.push(req.body.item);
+  res.redirect("/");
+});
+
+app.post("/week", function (req, res) {
+  weekList.push(req.body.item);
+  res.redirect("/week");
 });
 
 app.listen(3000, function () {
